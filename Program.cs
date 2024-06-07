34ab9f51642e34659c128babce1a0938b71550b8
c3developer <163186438+c3developer@users.noreply.github.com>
2024-06-07 13:03:03 +0300

    Create fields for data

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// цех участок(идукционная печь) время(начало конец и выбор дня)
+// напряжение (мин макс среднее) В таблице 3 столбца время hh.mm.ss ток 
+using System.Data;
+
+public class MarketReport
+{
+    public Workshop Workshop { get; set; }
+    public HistoryItem[] HistoryItem { get; set; }
+}
+// Переменные для шапки
+public class Workshop
+{
+    public String ShopName { get; set; }// цех
+    public String Sector { get; set; }// участок
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
+//Переменные для таблицы
+public class HistoryItem
+{
+    public decimal Tension { get; set; }
+    public DateTime DateTime { get; set; }
+}
+//Генератор данных
+public class MakeReporter
+{
+    public MarketReport GetReport()
+    {
+        return new MarketReport
+        {
+           Workshop = new Workshop
+           {
+               ShopName = "ТПЦ-2", Sector = "Индукционная Печь", StartDate = new DateTime(2024, 6, 7), EndDate = new DateTime(DateTime)
+           }
+        }
+    }
+}
